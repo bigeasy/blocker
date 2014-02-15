bfc4f0e7fa85cb4a6c5b2b944ae07f0e34f3b58f
Alan Gutierrez <alan@prettyrobots.com>
2014-02-15 13:56:47 -0800

    Remove dead code from `block.t.js`.
    See #7.

--- t/blocker/block.t.js
+++ t/blocker/block.t.js
@@ -10,35 +10,8 @@ require('proof')(1, function (equal) {
     pipe.write(buffer.slice(0, 1))
 
     blocker.block(2, function (buffer) {
-        equal(buffer.readUInt16BE(0), 0xaaaa, 'wait on next')
+        equal(buffer.readUInt16BE(0), 0xaaaa, 'block is ready')
     })
 
     pipe.write(buffer.slice(1))
-
-    return
-    function next() {
-        blocker.read(8, header)
-    }
-
-    function header (block) {
-        var type =  block.readUInt32(0)
-        var length =  block.readUInt32(4)
-        switch (type) {
-        case 0:
-            blocker.read(length, error)
-            break
-        case 1:
-            blocker.read(length, success)
-            break
-        }
-    }
-
-    function error (block) {
-        var code = block.getUInt32(0)
-    }
-
-    function success () {
-        var length = block.getUInt32(0)
-        var string = block.toString('utf8', 4, 4 + length)
-    }
 })
